330f056d4c42378f44f070a46c82247407bc8919
sunakan <sunakan03@gmail.com>
2020-11-21 11:21:34 +0000

    Svg4つめ

---
+++ app/src/MySvg4.elm
@@ -0,0 +1,29 @@
+module MySvg4 exposing (main)
+
+import Color
+import Html exposing (Html)
+import TypedSvg exposing (rect, svg)
+import TypedSvg.Attributes exposing (fill, stroke, viewBox)
+import TypedSvg.Attributes.InPx exposing (height, strokeWidth, width, x, y)
+import TypedSvg.Types exposing (Paint(..))
+import TypedSvg.Core exposing (Svg)
+
+
+myRectangle : Svg msg
+myRectangle =
+        rect
+            [ x 100
+            , y 100
+            , width 200
+            , height 200
+            , fill <| Paint Color.white
+            , strokeWidth 2
+            , stroke <| Paint Color.black
+            ]
+            []
+
+
+
+main : Html msg
+main =
+    svg [ viewBox 0 0 800 600 ] [ myRectangle ]
