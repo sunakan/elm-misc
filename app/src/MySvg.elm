8e2e6a48086bd95ac4da6b1b524138d0495ca840
sunakan <sunakan03@gmail.com>
2020-11-21 11:05:58 +0000

    svg

---
+++ app/src/MySvg.elm
@@ -0,0 +1,29 @@
+module MySvg exposing (main)
+
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+main =
+  svg
+    [ width "120"
+    , height "120"
+    , viewBox "0 0 120 120"
+    ]
+    [ rect
+        [ x "10"
+        , y "10"
+        , width "100"
+        , height "100"
+        , rx "15"
+        , ry "15"
+        , fill "green"
+        ]
+        []
+    , circle
+        [ cx "50"
+        , cy "50"
+        , r "50"
+        , fill "pink"
+        ]
+        []
+    ]
