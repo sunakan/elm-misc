ee5f9feae99672290f7203a2288942e45b69ca4b
sunakan <sunakan03@gmail.com>
2020-11-21 11:14:53 +0000

    Svg2つめ

---
+++ app/src/MySvg2.elm
@@ -0,0 +1,24 @@
+module MySvg2 exposing (main)
+
+import Color
+import Html exposing (Html)
+import TypedSvg exposing (circle, svg)
+import TypedSvg.Attributes exposing (cx, cy, fill, r, stroke, strokeWidth, viewBox)
+import TypedSvg.Types exposing (Paint(..), px)
+import TypedSvg.Core exposing (Svg)
+
+myCircle : Svg msg
+myCircle =
+    circle
+        [ cx (px 100)
+        , cy (px 100)
+        , r (px 30)
+        , fill <| Paint Color.blue
+        , strokeWidth (px 2)
+        , stroke <| Paint <| Color.rgba 0.8 0 0 0.5
+        ]
+        []
+
+main : Html msg
+main =
+    svg [ viewBox 0 0 800 600 ] [ myCircle ]
